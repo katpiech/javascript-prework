eb501f5b856dd57db862a2c91e3435405f0daf7e
Katarzyna Piechocińska <piechocinska.katarzyna@gmail.com>
2021-02-18 14:04:47 +0100

    Add random number function and else if

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
-var computerMove, playerMove;
-computerMove = 'kamień';
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
-playerMove = 'papier';
-printMessage('Mój ruch to:' + playerMove);
+var computerMove, randomNumber;
+randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log('wylosowana liczba to: ' + randomNumber);
+if (randomNumber == '1') {
+  computerMove = 'kamień';
+} else if (randomNumber == '2') {
+  computerMove = 'papier';
+} else if (randomNumber == '3') {
+  computerMove = 'nożyce';
+}
+printMessage('Mój ruch: ' + computerMove);
+computerMove = 'nieznany ruch';
