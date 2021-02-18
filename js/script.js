88a1e4fd1a1bc729d140344acf146354f91c5d26
Katarzyna Piechocińska <piechocinska.katarzyna@gmail.com>
2021-02-18 12:52:51 +0100

    Add playerMove variable

---
+++ js/script.js
@@ -0,0 +1,5 @@
+var computerMove, playerMove;
+computerMove = 'kamień';
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+playerMove = 'papier';
+printMessage('Mój ruch to:' + playerMove);
